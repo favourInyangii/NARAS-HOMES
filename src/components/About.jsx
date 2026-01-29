b448d68e3f7c78ca1350fabf11ccc65f832d0455
favour ololade <ololadefavourajayi@gmail.com>
2026-01-30 00:41:34 +0100

    project components creation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -30,22 +30,22 @@ const About = () => {
         <div className="flex flex-col items-center md:items-start mt-10 text-gray-600">
           <div className="grid grid-cols-2 gap-6 md:gap-10 w-full 2xl:pr-28">
             <div>
-              <p className="text-4xl font-medium text-gray-800">7+</p>
+              <p className="text-4xl font-medium text-gray-800">10+</p>
               <p>Years of Experience</p>
             </div>
 
             <div>
-              <p className="text-4xl font-medium text-gray-800">14+</p>
+              <p className="text-4xl font-medium text-gray-800">12+</p>
               <p>Projects Completed</p>
             </div>
 
             <div>
-              <p className="text-4xl font-medium text-gray-800">22+</p>
+              <p className="text-4xl font-medium text-gray-800">20+</p>
               <p>Mn. Sq. Ft Delivered</p>
             </div>
 
             <div>
-              <p className="text-4xl font-medium text-gray-800">29+</p>
+              <p className="text-4xl font-medium text-gray-800">25+</p>
               <p>Ongoing Projects</p>
             </div>
           </div>
