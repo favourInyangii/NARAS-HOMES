7cac7982ea74392d6e938c5d7818ed9361f0adc3
favour ololade <ololadefavourajayi@gmail.com>
2026-02-04 10:44:45 +0100

    testimonials section creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from './components/About'
 import Header from './components/Header'
 import Projects from './components/Projects'
+import Testimonials from './components/Testimonials'
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Header />
       <About />
       <Projects />
+      <Testimonials />
     </div>
   )
 }
---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,19 @@
+import { testimonialsData } from "../assets/assets"
+const Testimonials = () => {
+    return (
+        <div className="container mx-auto py-10 lg:px-32 w-full overflow-hidden">
+            <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">Customer<span className="underline underline-offset-4 decoration-1 under font-light">Testimonials</span></h1>
+            <p className="text-center text-gray-500 mb-12 max-w-80 mx-auto">Stories from those who found a home with us</p>
+
+            <div className="flex flex-wrap justify-center gap-8">
+                {testimonialsData.map((testimonial, index) => (
+                    <div key={index} className="max-w-[340px] border shadow-lg rounded px-8 py-12 text-center">
+                        <img className="w-20 h-20 rounded-full mx-auto mb-4" src={testimonial.image} alt={testimonial.alt} />
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Testimonials
