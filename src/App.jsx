bad8e90586e79a3586398d510e707ba4cfe85a18
favour ololade <ololadefavourajayi@gmail.com>
2026-02-07 20:26:26 +0100

    Fix Contact import case to match filename

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import About from './components/About'
+import Contact from './components/Contact'
 import Header from './components/Header'
 import Projects from './components/Projects'
 import Testimonials from './components/Testimonials'
-import Contact from './components/contact'
+
 
 const App = () => {
   return (
