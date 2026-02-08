d4c0b60dd1e58359671f9a42fb61cb1f43315e3b
favour ololade <ololadefavourajayi@gmail.com>
2026-02-08 09:06:14 +0100

    Trigger redeploy to apply env variables

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Projects from './components/Projects'
 import Testimonials from './components/Testimonials'
 
 
+
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
