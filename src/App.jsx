755b7a7a5d907813bcb164ee7c62b58f5adb8240
favour ololade <ololadefavourajayi@gmail.com>
2026-02-12 19:15:39 +0100

    footer section created

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import About from './components/About'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 import Header from './components/Header'
 import Projects from './components/Projects'
 import Testimonials from './components/Testimonials'
@@ -17,6 +18,7 @@ const App = () => {
       <Projects />
       <Testimonials />
       <Contact />
+      <Footer />
     </div>
   )
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,7 +33,7 @@ const Contact = () => {
     };
 
     return (
-        <div className="text-center p-6 py-20 lg:px-32 w-full overflow-hidden" id="Contact">
+        <div className="text-center p-6 py-20 lg:px-32 w-full overflow-hidden" id="contact">
             <h1 className="text-2xl sm:text-4xl font-bold mb-2">
                 Contact{" "}
                 <span className="underline underline-offset-4 decoration-1 font-light">
